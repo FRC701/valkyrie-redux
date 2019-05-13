a2a18c56b406729c092ed6231d565aeb8d69205d
Duane Murphy <duane@vandenrobotics.com>
2019-05-13 16:37:07 -0700

    Our First Test: Chassis
    * Add the test 
    * Add subsystem
    
    The subsystem has not been modified at all.

---
+++ src/test/cpp/subsystems/Chassis.cpp
@@ -0,0 +1,20 @@
+// test/cpp/subsystems/Chassis.cpp
+// Testing the Chassis Subsystem
+
+#include "subsystems/Chassis.h"
+
+#include <gtest/gtest.h>
+
+namespace robovikes {
+namespace valkyrie {
+namespace testing {
+namespace {
+
+TEST(ChassisTest, ConstructSubsystem) {
+    Chassis chassis;
+}
+
+} // namespace
+} // namespace testing
+} // namespace valkyrie
+} // namespace robovikes
