95f796d792682f71171f3d7f9843bd458943baf2
Mr. Murphy <dmurphy@vandenrobotics.com>
2019-05-13 23:43:42 -0700

    4 Motor Tank Drive
    Use 4 motors (externally).
    Add the TankDrive support with squared inputs and a dead band.
    
    Note the previous errors -- The right side should have been going in reverse for a tank drive.
    
    Note the offset for output from input for TankDrive due to the dead band and squared inputs.

--- src/test/cpp/subsystems/Chassis.cpp
+++ src/test/cpp/subsystems/Chassis.cpp
@@ -5,12 +5,15 @@
 
 #include "../../include/NullSpeedController.h"
 #include "../../include/MockSpeedController.h"
+#include <frc/SpeedControllerGroup.h>
 #include <gtest/gtest.h>
 
 namespace robovikes {
 namespace valkyrie {
 namespace testing {
 namespace {
+
+using ::testing::DoubleNear;
 
 // Use Twine instead of std::string because it's WPI.
 const wpi::Twine kChassisSubsystemName = "Chassis";
@@ -46,7 +49,7 @@ TEST_F(ChassisTest, Drive) {
     MockSpeedController right;
 
     EXPECT_CALL(left, Set(1.0));
-    EXPECT_CALL(right, Set(1.0));
+    EXPECT_CALL(right, Set(-1.0));
     Chassis chassis(kChassisSubsystemName, left, right);
 
     // Action
@@ -58,6 +61,62 @@ TEST_F(ChassisTest, Drive) {
     // Mocks for left and right will assert
 }
 
+// Chassis should be 4 motors. 
+TEST_F(ChassisTest, Drive4Motor) {
+    // Arrange
+    using MockSpeedController = robovikes::testing::MockSpeedController;
+    MockSpeedController leftFront;
+    MockSpeedController rightFront;
+    MockSpeedController leftRear;
+    MockSpeedController rightRear;
+    frc::SpeedControllerGroup left(leftFront, leftRear);
+    frc::SpeedControllerGroup right(rightFront, rightRear);
+
+    EXPECT_CALL(leftFront, Set(1.0));
+    EXPECT_CALL(rightFront, Set(-1.0));
+    EXPECT_CALL(leftRear, Set(1.0));
+    EXPECT_CALL(rightRear, Set(-1.0));
+    Chassis chassis(kChassisSubsystemName, left, right);
+
+    // Action
+    constexpr double kDriveLeft = 1.0;
+    constexpr double kDriveRight = 1.0;
+    chassis.Drive(kDriveLeft, kDriveRight);
+
+    // Assert
+    // Mocks for left and right will assert
+}
+
+// And we would like to use the
+// support of the DifferentialDrive capabilities. 
+TEST_F(ChassisTest, DriveDifferential) {
+    // Arrange
+    using SpeedController = robovikes::testing::MockSpeedController;
+    SpeedController leftFront;
+    SpeedController rightFront;
+    SpeedController leftRear;
+    SpeedController rightRear;
+
+    constexpr double kDriveLeft = 0.5;
+    constexpr double kDriveRight = 0.5;
+    constexpr double kOutputLeft = 0.2399; // squared and scaled for the deadband
+    constexpr double kOutputRight = -0.2399; // squared and scaled for the deadband
+
+    EXPECT_CALL(leftFront, Set(DoubleNear(kOutputLeft, 0.0001)));
+    EXPECT_CALL(rightFront, Set(DoubleNear(kOutputRight, 0.0001)));
+    EXPECT_CALL(leftRear, Set(DoubleNear(kOutputLeft, 0.0001)));
+    EXPECT_CALL(rightRear, Set(DoubleNear(kOutputRight, 0.0001)));
+
+    frc::SpeedControllerGroup left(leftFront, leftRear);
+    frc::SpeedControllerGroup right(rightFront, rightRear);
+    Chassis chassis(kChassisSubsystemName, left, right);
+
+    // Action
+    chassis.Drive(kDriveLeft, kDriveRight);
+
+    // Assert
+    // Mocks for left and right will assert
+}
 
 } // namespace
 } // namespace testing
